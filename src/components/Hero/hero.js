a8d2d62f5f56e499d7892ac85efcaf0af40634cc
Dane Petersen <dane.m.petersen@gmail.com>
2021-04-21 14:22:27 -0500

    texting out some animations with framer and additional layouts to glass div - i think i like this better

--- src/components/Hero/hero.js
+++ src/components/Hero/hero.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { motion } from "framer-motion";
+import React, { useState, useEffect } from "react";
+import { motion, useAnimation } from "framer-motion";
 import {
     BrowserRouter as Router,
     useHistory,
@@ -7,6 +7,19 @@ import {
   } from "react-router-dom";
 
   const Hero = () => {
+  const controls = useAnimation()
+
+  const list = { hidden: { opacity: 0 } }
+const item = { hidden: { x: -10, opacity: 0 } }
+
+  useEffect(() => {
+    controls.start(i => ({
+      opacity: 1,
+      x: 100,
+      transition: { delay: i * 9 },
+    }))
+  }, [])
+
   return (
     <div className="hero-container">
       <div className="cube"></div>
@@ -22,10 +35,26 @@ import {
             <div className="yellow-dot"></div>
           </div>
         </div>
+
+
+        <motion.ul animate="hidden" variants={list}>
+    <motion.li variants={item} />
+    <motion.li variants={item} />
+    <motion.li variants={item} />
+  </motion.ul>
+
+
+        <ul>
+        <motion.li custom={0} animate={controls} />
+        <motion.li custom={1} animate={controls} />
+        <motion.li custom={2} animate={controls} />
+        </ul>
+
+
         <div className="cards">
-          <div className="card">
+          <motion.div custom={0} animate={controls} className="card">
             <h5>Example</h5>
-          </div>
+          </motion.div>
           <div className="card">
             <h5>Example</h5>
           </div>
