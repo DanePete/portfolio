6c5834f1f3eac1dce52cb25ac80226e20282e630
Dane Petersen <dane.m.petersen@gmail.com>
2021-04-21 11:49:37 -0500

    removed create-react-app defaults and restructured component and scss directories

---
+++ src/components/Hero/hero.js
@@ -0,0 +1,17 @@
+import React from "react";
+import {
+    BrowserRouter as Router,
+    useHistory,
+    useParams
+  } from "react-router-dom";
+
+  const Hero = () => {
+  return (
+    <div className="hero-container">
+      
+    </div>
+  );
+
+};
+
+export default Hero;
