ce55c77d5aa23b0434a1329a5d81727b388115fe
Dane Petersen <dane.m.petersen@gmail.com>
2021-04-21 14:05:29 -0500

    working out my idea for a terminal/phone type notification animation on page load

--- src/components/Hero/hero.js
+++ src/components/Hero/hero.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { motion } from "framer-motion";
 import {
     BrowserRouter as Router,
     useHistory,
@@ -14,7 +15,31 @@ import {
       <div className="cube"></div>
       <div className="cube"></div>
       <div className="cube"></div>
-      <section className="glass"></section>
+      <section className="glass">
+        <div className="statusBar">
+          <div className="notiication-center">
+            <div className="red-dot"></div>
+            <div className="yellow-dot"></div>
+          </div>
+        </div>
+        <div className="cards">
+          <div className="card">
+            <h5>Example</h5>
+          </div>
+          <div className="card">
+            <h5>Example</h5>
+          </div>
+          <div className="card">
+            <h5>Example</h5>
+          </div>
+          <div className="card">
+            <h5>Example</h5>
+          </div>
+          <div className="card">
+            <h5>Example</h5>
+          </div>          
+        </div>
+      </section>
     </div>
   );
 
