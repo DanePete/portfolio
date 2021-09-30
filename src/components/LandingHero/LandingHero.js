c719bec4144fbf8806a6efe71d86a8fe71a624de
dane <dane.m.petersen@gmail.com>
2021-09-29 22:09:03 -0500

    improvements to aimation

--- src/components/LandingHero/LandingHero.js
+++ src/components/LandingHero/LandingHero.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import "animate.css"
+import mojs from '@mojs/core'
 import { motion, useAnimation, AnimatePresence, useViewportScroll } from "framer-motion";
 import {
     BrowserRouter as Router,
@@ -9,11 +10,27 @@ import {
 
   const LandingHero = () => {
 
+
+    const bouncyCircle = new mojs.Shape({
+      // parent:       '#bouncyCircle',
+      shape:        'circle',
+      fill:         {'#F64040': '#FC46AD'},
+      radius:       {20: 80},
+      duration:     2000,
+      isYoyo:       true,
+      isShowStart:  true,
+      easing:       'elastic.inout',
+      repeat:       1,
+    });
+    
+    bouncyCircle.play()
+
   const controls = useAnimation()
   const list = { hidden: { opacity: 0 } }
   const item = { hidden: { x: -10, opacity: 0 } }
   const { scrollYProgress } = useViewportScroll();
   const history = useHistory();
+  
 
   function goToGitHub() {
     window.location.assign('https://github.com/DanePete');
@@ -29,20 +46,21 @@ import {
 
   return (
     <div className="landing-hero-container">
+      <div id="bouncyCircle"></div>
       <div className="infinity-container">
-        <div class="frame animate__animated animate__fadeOutUp animate__delay-5s"></div>
-        <div class="frame animate__animated animate__fadeOutUp animate__delay-5s"></div>
-        <div class="frame animate__animated animate__fadeOutUp animate__delay-5s"></div>
-        <div class="frame animate__animated animate__fadeOutUp animate__delay-5s"></div>
-        <div class="frame animate__animated animate__fadeOutUp animate__delay-3s"></div>
-        <div class="frame animate__animated animate__fadeOutUp animate__delay-3s"></div>
-        <div class="frame animate__animated animate__fadeOutUp animate__delay-3s"></div>
-        <div class="frame animate__animated animate__fadeOutUp animate__delay-3s"></div>
-        <div class="frame animate__animated animate__fadeOutUp animate__delay-7s"></div>
-        <div class="frame animate__animated animate__fadeOutUp animate__delay-7s"></div>
-        <div class="frame animate__animated animate__fadeOutUp animate__delay-7s"></div>
-        <div class="frame animate__animated animate__fadeOutUp animate__delay-7s"></div>
-        <div class="frame animate__animated animate__fadeOutUp animate__delay-3s"></div>
+        <div class="frame animate__animated animate__jello animate__delay-16s animate__infinite"></div>
+        <div class="frame animate__animated animate__jello animate__delay-15s animate__infinite"></div>
+        <div class="frame animate__animated animate__jello animate__delay-14s animate__infinite"></div>
+        <div class="frame animate__animated animate__jello animate__delay-13s animate__infinite"></div>
+        <div class="frame animate__animated animate__jello animate__delay-12s animate__infinite"></div>
+        <div class="frame animate__animated animate__jello animate__delay-11s animate__infinite"></div>
+        <div class="frame animate__animated animate__jello animate__delay-10 animate__infinites"></div>
+        <div class="frame animate__animated animate__jello animate__delay-9s animate__infinite"></div>
+        <div class="frame animate__animated animate__jello animate__delay-8s animate__infinite"></div>
+        <div class="frame animate__animated animate__jello animate__delay-7s animate__infinite"></div>
+        <div class="frame animate__animated animate__jello animate__delay-6s animate__infinite"></div>
+        <div class="frame animate__animated animate__jello animate__delay-5s animate__infinite"></div>
+        <div class="frame animate__animated animate__jello animate__delay-4s animate__infinite"></div>
       </div>
     </div>
   );
