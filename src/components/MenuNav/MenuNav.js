118ca967731e59e2b200fc461f51185416130fa5
dane <dane.m.petersen@gmail.com>
2021-10-15 08:39:25 -0500

    added linked-in-badge

--- src/components/MenuNav/MenuNav.js
+++ src/components/MenuNav/MenuNav.js
@@ -26,6 +26,9 @@ class MenuNav extends React.Component {
         <Link className="navLink" to="/contact">
           Contact
         </Link>
+
+        <div className="badge-base LI-profile-badge" data-locale="en_US" data-size="medium" data-theme="dark" data-type="HORIZONTAL" data-vanity="dane-petersen" data-version="v1"><a className="badge-base__link LI-simple-link" href="https://www.linkedin.com/in/dane-petersen?trk=profile-badge"></a></div>
+
       </Menu>
     );
   }
